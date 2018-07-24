b8f536380eb0abb4d22c113f46f04dbf9d456c12
Erick I Barcenas <36802371+barcenitas@users.noreply.github.com>
2018-07-24 10:22:55 -0500

    Primeros pasos Angular

---
+++ WEB/CursoAngular4-backend/index.php
@@ -0,0 +1,21 @@
+<?php
+require_once 'vendor/autoload.php';
+
+$app = new \Slim\Slim();
+
+$db = new mysqli('localhost', 'root','','curso_angular4');
+
+$app->get("/pruebas",function() use($app, $db){
+	echo "Hola mundo desde Slim PHP";
+	var_dump($db);
+});
+
+$app->get("/probando",function() use($app){
+	echo "Hola, otro texto";
+});
+
+$app ->post('/productos', function() use($app,$db){
+
+});
+
+$app->run();
